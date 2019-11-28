f83422d6f975cd1cd6dc58323c7d8443957bae51
sophz55 <sophiazheng5@gmail.com>
2019-11-28 11:50:27 -0500

    milestone 6

---
+++ housing-explorations-app/app.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(markdown)
+
+# Define UI for application that draws a histogram
+ui <- navbarPage("Milestone 6",
+                 tabPanel("About", includeMarkdown("about.md")))
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
