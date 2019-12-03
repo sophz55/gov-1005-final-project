7d7fc99cfa4969fe29fb50f16f63c8cb3634793e
sophz55 <sophiazheng5@gmail.com>
2019-12-03 02:06:55 -0500

    formatting explore graph

--- housing-explorations-app/app.R
+++ housing-explorations-app/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(markdown)
+library(tidyverse)
 
 all_housing_data <- read_csv("all_housing_data.csv")
 
@@ -28,7 +29,23 @@ ui <- navbarPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
   output$line_plot <- renderPlot({
+    type <- case_when(
+      input$measure == "mean_zhvi" ~ "ZHVI",
+      input$measure == "mean_sale_price" ~ "Sale Price",
+      input$measure == "mean_zri" ~ "ZRI",
+      input$measure == "mean_rental_price" ~ "Rental Price",
+    )
+    
+    all_housing_data$metro <- factor(all_housing_data$metro,
+                                     levels = c("New York-Newark-Jersey City", 
+                                                "San Francisco-Oakland-Hayward", 
+                                                "Seattle-Tacoma-Bellevue"),
+                                     labels = c("New York Area",
+                                                "San Francisco Area",
+                                                "Seattle Area"))
+    
     all_housing_data %>%
       filter(value_type == input$measure) %>% 
       drop_na() %>% 
@@ -36,7 +53,12 @@ server <- function(input, output) {
                  y = mean_value,
                  group = metro,
                  color = metro)) +
-      geom_line()
+      geom_line() +
+      scale_y_continuous(labels = scales::dollar) +
+      labs(title = (paste("Average", type, "By Month")),
+           x = "Year",
+           y = paste("Mean", type),
+           color = "Region")
   })
 }
 
